33c3391c5e57c436c41f0ff8296dbbc30ceaefaa
Nasan Soonthondachar <63160070@go.buu.ac.th>
2022-02-26 21:45:15 +0700

    Make info can be add only when have value,Make keyboard only have number in amount,Make number 2 decimal point

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -19,12 +19,13 @@ class TransactionList extends StatelessWidget {
                 Container(
                   padding: EdgeInsets.all(10),
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  child: Text('\$${transactions[index].amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      )),
+                  child:
+                      Text('\$${transactions[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          )),
                   decoration: BoxDecoration(
                       border: Border.all(
                     color: Colors.purple,
