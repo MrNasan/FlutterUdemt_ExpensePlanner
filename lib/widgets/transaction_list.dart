97c4cd721c843756a6a7fd971b68c53df5cab649
Nasan Soonthondachar <63160070@go.buu.ac.th>
2022-02-26 14:29:20 +0700

    Change SingleChildScrollView to ListView.builder to better performance

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,46 +11,45 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    child: Text('\$${tx.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        )),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    )),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18),
-                      ),
-                      Text(
-                        DateFormat('dd/MM/yyyy').format(tx.date),
-                        style: TextStyle(fontSize: 16, color: Colors.grey),
-                      )
-                    ],
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  child: Text('\$${transactions[index].amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      )),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      DateFormat('dd/MM/yyyy').format(transactions[index].date),
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    )
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
       ),
     );
   }
